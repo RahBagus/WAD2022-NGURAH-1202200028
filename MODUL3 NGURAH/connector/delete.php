d985765437149742ce17f03ce7866a9e13e5aacc
RahBagus <putraadnyana33@gmail.com>
2022-11-22 08:08:18 +0700

    MODUL3 NGURAH

--- MODUL3 NGURAH/connector/delete.php
+++ MODUL3 NGURAH/connector/delete.php
@@ -3,9 +3,9 @@ require '../connector/connector.php';
 
 $id = $_GET['id'];
 
-$sql = "DELETE FROM showroom_nama_table WHERE id_mobil = $id";
+$mysql  ="DELETE FROM showroom_nama_table WHERE id_mobil = $id";
 
-if (mysqli_query($connector, $sql)) {
+if (mysqli_query($connector, $mysql )) {
   header("location: ../pages/ListCar-Ngurah.php?message=delete");
 } else {
   echo "Gagal";
